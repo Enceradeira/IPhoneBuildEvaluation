b61677f9c322679e45de29744ac1321f56584c15
Jorg Jenni <jorg.jenni@jennius.co.uk>
2014-06-05 13:22:30 +0100

    plain ruby-script using Appium added. Ruby interface to Appium works

--- HelloWorldAppAcceptanceTests/simple_test.rb
+++ HelloWorldAppAcceptanceTests/simple_test.rb
@@ -1,18 +1,34 @@
 require 'appium_lib'
 
-APP_PATH = '../HelloWorldApp/Build/Products/Release-iphonesimulator/HelloWorldApp.app'
+module Tests
+  module HelloWorld
 
-desired_caps = {
-    caps:       {
-        platformName:  'iOS',
-        versionNumber: '7.1',
-        app:           APP_PATH,
-    },
-    appium_lib: {
-        sauce_username:   nil, # don't run on Sauce
-        sauce_access_key: nil
+    APP_PATH = '../HelloWorldApp/Build/Products/Release-iphonesimulator/HelloWorldApp.app'
+
+    desired_caps = {
+        caps: {
+            platformName: 'iOS',
+            versionNumber: '7.1',
+            app: APP_PATH,
+        },
+        appium_lib: {
+            sauce_username: nil, # don't run on Sauce
+            sauce_access_key: nil
+        }
     }
-}
 
 # Start the driver
-Appium::Driver.new(desired_caps).start_driver
+    Appium::Driver.new(desired_caps).start_driver
+    Appium.promote_singleton_appium_methods Tests
+
+    begin
+
+      button('click me').click
+
+      wait { text 'Hello World' }
+
+    ensure
+      driver_quit
+    end
+  end
+end
