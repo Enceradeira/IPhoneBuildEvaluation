e4bcc6ce2d0b9bd97484b89b05586cadec7832de
Jorg Jenni <jorg.jenni@jennius.co.uk>
2014-06-09 19:58:32 +0100

    Hello World-Alert added

---
+++ app/HelloWorldApp/JENHelloWorldControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  JENHelloWorldControllerViewController.h
+//  HelloWorldApp
+//
+//  Created by Jorg on 09/06/2014.
+//  Copyright (c) 2014 co.uk.jennius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JENHelloWorldControllerViewController : UIViewController
+
+@end
